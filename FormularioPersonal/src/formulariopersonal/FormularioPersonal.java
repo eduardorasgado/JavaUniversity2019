6fa54cd29e8cdc7bf2e87b2e8d2e612e4635b41e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-03-16 15:42:56 -0600

    Manejo de errores en el main y manejo de mensajes de error

--- FormularioPersonal/src/formulariopersonal/FormularioPersonal.java
+++ FormularioPersonal/src/formulariopersonal/FormularioPersonal.java
@@ -6,7 +6,13 @@
 package formulariopersonal;
 
 import com.eduardocode.app.UI.FormularioPrincipal;
+import com.eduardocode.app.model.Alumno;
 import com.eduardocode.app.service.AlumnoServiceImp;
+import com.toedter.calendar.JCalendar;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import javax.swing.JTextField;
 
 /**
  *
@@ -22,17 +28,59 @@ public class FormularioPersonal {
         // TODO code application logic here
         // agregando el servicio
         AlumnoServiceImp alumnoService = new AlumnoServiceImp();
-    
-        FormularioPrincipal form = new FormularioPrincipal();
+        FormularioPrincipal alumnoForm = new FormularioPrincipal();
         
-        form.setVisible(true);
-        form.setLocationRelativeTo(null);
+        alumnoForm.setVisible(true);
+        alumnoForm.setLocationRelativeTo(null);
         
         // if(numControl != 0 && edadPersona != 0) {
             // guardar como una entidad
         //    Alumno alumnoSaved = alumnoService.saveAlumno(nombrePersona, edadPersona, numControl, fechaIngreso);
         //    this.showAlumnoCreated(alumnoSaved);
         //}
-    }
-    
+        
+        alumnoForm.getBtnSalir().addActionListener( (ActionListener) -> {
+            System.err.println("SALIR");
+            System.exit(0);
+        });
+        
+        alumnoForm.getBtnGuardar().addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // recaudando los datos de los campos
+                String nombre = alumnoForm.getNombreText();
+                int edad = 0;
+                int numControl = 0;
+
+                JTextField edadContainer = alumnoForm.getEdadText();
+                JTextField numControlContainer = alumnoForm.getNumControlText();
+                JCalendar fechaIngresoContainer = alumnoForm.getFechaIngresoText();
+                
+                // validacion y obtencion de campos, conversion a enteros
+                if ("".equals(nombre)) {
+                    alumnoForm.showInputError(alumnoForm, "Inserte un nombre");
+                }
+
+                try {
+                    edad = Integer.parseInt(edadContainer.getText());
+                } catch(NumberFormatException error) {
+                    alumnoForm.showInputError(alumnoForm, "inserte una edad valida");
+                }
+
+                try {
+                    numControl = Integer.parseInt(numControlContainer.getText());
+                } catch(NumberFormatException error) {
+                    alumnoForm.showInputError(alumnoForm, "inserte un numero de control valido");
+                }
+
+                Date fechaIngreso = fechaIngresoContainer.getCalendar().getTime();
+
+                // si se cumple la validacion
+                if(edad != 0 && numControl != 0 && !"".equals(nombre)) {
+                    Alumno newAlumno = alumnoService.saveAlumno(nombre, edad, numControl, fechaIngreso);
+                    alumnoForm.showAlumnoCreated(newAlumno);
+                }
+            }
+        });
+    }    
 }
