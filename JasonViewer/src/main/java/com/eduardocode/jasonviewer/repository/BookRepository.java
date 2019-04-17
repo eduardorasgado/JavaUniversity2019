c05ce7e38104e95834bcc588fe8d63a14a280b65
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-16 21:42:48 -0500

    Implementing all methods in book repository

--- JasonViewer/src/main/java/com/eduardocode/jasonviewer/repository/BookRepository.java
+++ JasonViewer/src/main/java/com/eduardocode/jasonviewer/repository/BookRepository.java
@@ -14,38 +14,54 @@ public class BookRepository implements JVRepository<Book> {
 	
 	@Override
 	public ArrayList<Book> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return books;
 	}
 
 	@Override
 	public Book findByIndex(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if(index >=0 && index < books.size()) {
+			return books.get(index);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
-	public int getResourceIndex(Book resource) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getResourceIndex(Book book) {
+		for(int i = 0;i < books.size(); i++) {
+			if(books.get(i).getTitle().equals(book.getTitle())) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	@Override
-	public void insert(Book resource) {
-		// TODO Auto-generated method stub
+	public void insert(Book book) {
+		if(book != null) {
+			books.add(book);
+		}
 		
 	}
 
 	@Override
 	public boolean delete(int index) {
-		// TODO Auto-generated method stub
-		return false;
+		if(index >=0 && index < books.size()) {
+			books.remove(index);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
-	public boolean update(int index, Book resource) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(int index, Book book) {
+		if((index >= 0 && index < books.size()) && book != null) {
+			books.set(index, book);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
