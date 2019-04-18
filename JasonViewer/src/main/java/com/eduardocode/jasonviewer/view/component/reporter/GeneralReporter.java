153683be176d736abb3032af00f741b81f0f4a93
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-18 17:48:11 -0500

    general reporter created and extends from generic reporter

---
+++ JasonViewer/src/main/java/com/eduardocode/jasonviewer/view/component/reporter/GeneralReporter.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.eduardocode.jasonviewer.view.component.reporter;
+
+/**
+ * @author cheetos
+ *
+ */
+public class GeneralReporter extends GenericReporterComponent {
+
+	@Override
+	protected void showMainMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interact() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void showMovieReport() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void showSerieReport() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void showBookReport() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
