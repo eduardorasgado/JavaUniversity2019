6a8ec6621eae472cb6193524dd3a7efd6bd64560
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-18 22:37:57 -0500

    Documenting the models p2.

--- JasonViewer/src/main/java/com/eduardocode/jasonviewer/model/IReadable.java
+++ JasonViewer/src/main/java/com/eduardocode/jasonviewer/model/IReadable.java
@@ -6,11 +6,29 @@ package com.eduardocode.jasonviewer.model;
 import java.util.Date;
 
 /**
+ * <h1>IReadable</h1>
+ * 
  * Interfaz que representa un patron util para los modelos que sean de lectura
  * @author cheetos
+ * @version 1.1
+ * @since 2019
  *
  */
 public interface IReadable {
+	/**
+	 * Metodo que permite crear una logica para la captacion de una fecha de 
+	 * ingreso o entrada
+	 * @param dateI Fecha de ingreso
+	 * @return Devuelve una fecha exacta
+	 */
 	Date startToSee(Date dateI);
+	
+	/**
+	 * Metodo que permite crear una logica para determinar el tiempo total que
+	 * toma la lectura de un readable a partir de una fecha de ingreso y una 
+	 * fecha de cierre
+	 * @param dateI Fecha de ingreso
+	 * @param dateF Fecha de cierre
+	 */
     void stoptToSee(Date dateI, Date dateF);
 }
