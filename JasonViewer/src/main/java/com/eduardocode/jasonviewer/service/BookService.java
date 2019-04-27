85905784afa1af71b5889e3ed9f95079d13ea3bc
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-26 20:21:19 -0500

    Playing book's pages in book service, in play resource method

--- JasonViewer/src/main/java/com/eduardocode/jasonviewer/service/BookService.java
+++ JasonViewer/src/main/java/com/eduardocode/jasonviewer/service/BookService.java
@@ -5,8 +5,10 @@ package com.eduardocode.jasonviewer.service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 import com.eduardocode.jasonviewer.model.Book;
+import com.eduardocode.jasonviewer.model.Book.Page;
 import com.eduardocode.jasonviewer.repository.BookRepository;
 
 /**
@@ -18,6 +20,7 @@ import com.eduardocode.jasonviewer.repository.BookRepository;
 public class BookService extends WatchableService implements IWatchableService<Book> {
 
 	private BookRepository bookRepository;
+	private Scanner scanner;
 	
 	public BookService() {
 		bookRepository = new BookRepository();
@@ -70,17 +73,64 @@ public class BookService extends WatchableService implements IWatchableService<B
 	public void playResource(int index) {
 		// libro que va a ser actualizado
 		Book book = bookRepository.getAll().get(index);
-		book.setRead(true);
 		
 		Date dateInicio = book.startToSee(new Date());
 		
-		this.simulateTimeLapse(1000);
+		//this.simulateTimeLapse(1000);
+		
+		book = this.playPages(book);
 		
 		// el tiempo que fue leido el libro
 		book.stoptToSee(dateInicio, new Date());
 		
 		// guardando el recurso
 		this.bookRepository.update(index, book);
+	}
+	
+	private Book playPages(Book book) {
+		ArrayList<Book.Page> totalPages = book.getPages();
+		// lectura de las paginas dentro de un libro
+		scanner = new Scanner(System.in);
+		int i = 0;
+		do {
+			Page actualPage = totalPages.get(i);
+			
+			System.out.println("Page: "+actualPage.getNumber());
+			System.out.println(actualPage.getContent());
+			System.out.println("....");
+			
+			if(actualPage.getNumber() == totalPages.size()) {
+				// si la pagina en la que estamos es igual al numero ttal de 
+				//paginas entonces ya leimos todo el libro
+				book.setRead(true);
+			} else {
+				System.out.println("1. Ir a la sig pagina");
+			}
+			
+			if(actualPage.getNumber() > 1) {
+				System.out.println("2. Ir a la pagina anterior");
+			}
+			System.out.println("0. cerrar libro");
+		
+			// validacion e interpretacion de la respuesta
+			int response = Integer.valueOf(scanner.nextLine());
+			
+			if(response == 1)  {
+				i++;
+			}
+			else if(response == 2 && actualPage.getNumber() > 1) {
+				i--;
+			}
+			else if(response == 0) {
+				System.out.println("Cerrando libro");
+				break;
+			}
+			else {
+				System.out.println("Respuesta invalida");
+			}
+		} while(i < book.getPages().size());
+		
+		return book;
 	}
 
 }
