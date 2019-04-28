d06a8ef1465c292be0248271a0b619b3763a345d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-27 23:07:38 -0500

    Property loader actually reads and maps correctly all the yaml properties.

--- JasonViewer/src/main/java/com/eduardocode/jasonviewer/utils/PropertiesLoader.java
+++ JasonViewer/src/main/java/com/eduardocode/jasonviewer/utils/PropertiesLoader.java
@@ -7,24 +7,41 @@ import java.nio.file.Paths;
 
 import org.yaml.snakeyaml.Yaml;
 
+import com.eduardocode.jasonviewer.db.configuration.vo.Configuration;
+
 import lombok.Data;
 
+/**
+ * <h1>PropertiesLoader</h1>
+ * Clase que abstrae la lectura de un archivo de propiedades yaml y puede traer
+ * estas propiedades y mapearlas en objetos virtuales de configuracion
+ * @author Eduardo Rasgado Ruiz
+ * @see Configuration
+ *
+ */
 @Data
 public class PropertiesLoader {
 	private final String locationFile = "src\\main\\resources\\application.yaml";
 	private Yaml yaml;
 	
-	private String envPassword;
+	// objeto de mapeo principal del yaml
+	Configuration configuration = null;
 	
 	public PropertiesLoader() {
 		this.yaml = new Yaml();
 		this.initYamlConfiguration();
 	}
 	
+	/**
+	 * Metodo que crea un stream de entrada para leer una direccion de archivo
+	 * en el sistema operativo, con esta misma y tomando de base un objeto Yaml
+	 * de la libreria snakeyaml hace un mapeo de propiedades hacia un
+	 * Objeto de tipo Configuracion.
+	 */
 	private void initYamlConfiguration() {
 		try{
 			InputStream in = Files.newInputStream(Paths.get(locationFile));
-			this.envPassword = yaml.load(in);
+			configuration = yaml.loadAs(in, Configuration.class);
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
